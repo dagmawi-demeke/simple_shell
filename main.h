2e0fea513ffa836fe386926e22abfeedc6019c0f
dagmawi-demeke <67114822+dagmawi-demeke@users.noreply.github.com>
2022-08-11 01:15:57 +0300

    Update main.h

--- main.h
+++ main.h
@@ -158,9 +158,6 @@ ssize_t get_line(char **lineptr, size_t *n, FILE *stream);
 /* exec_line */
 int exec_line(data_shell *datash);
 
-int print_alias(data_of_program *data, char *alias);
-char *get_alias(data_of_program *data, char *name);
-int set_alias(char *alias_string, data_of_program *data);
 
 /* cmd_exec.c */
 int is_cdir(char *path, int *i);
